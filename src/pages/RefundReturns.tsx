f5a6b88c967bc8ea6426db881e1ab00b2ba37ab7
Bijaya-nanda <140945055+Bijaya-nanda@users.noreply.github.com>
2025-08-12 17:56:01 +0530

    Update RefundReturns.tsx

--- src/pages/RefundReturns.tsx
+++ src/pages/RefundReturns.tsx
@@ -6,7 +6,7 @@ export default function RefundReturns() {
       <div className="prose prose-lg">
         <section className="mb-8">
           <p className="mb-4">
-            Since https://[YourDomainName].com/ offers non-tangible irrevocable goods we do not issue refunds once the order is accomplished and the product download will be available and accessible via mail or download. The nature of digital media means that once you have downloaded a product there's really no way to 'return' it. So all payments at this website are non refundable and any request or dispute will be rejected without any communication.
+            Since https://corekits.store// offers non-tangible irrevocable goods we do not issue refunds once the order is accomplished and the product download will be available and accessible via mail or download. The nature of digital media means that once you have downloaded a product there's really no way to 'return' it. So all payments at this website are non refundable and any request or dispute will be rejected without any communication.
           </p>
           <p className="mb-4">
             As a customer you are responsible for understanding this upon purchasing any item at our site. There will be no refund if the product or service is downloadable or usable online in real-time and our records shows that product or service has been downloaded or used through our system.
@@ -22,7 +22,7 @@ export default function RefundReturns() {
             It may happen so that you are having problems while downloading the product or its unzipping. Claims regarding such issues must be submitted within 24 hours. If you do not properly contact us during this period, you agree that we may construe silence as a successful download of the product with no further right of redress or refund for a "download issue" reason.
           </p>
           <p className="mb-4">
-            In case the if you have not able unzip the product, please contact us immediately. We will be happy to mail you the product to download. please contact us on <a href="mailto:hello@yourdomain.com" className="text-blue-600 hover:underline">hello@[YourDomainName].com</a> for the same.
+            In case the if you have not able unzip the product, please contact us immediately. We will be happy to mail you the product to download. please contact us on <a href="mailto:corekits@gmail.com" className="text-blue-600 hover:underline">corekits@gmail.com</a> for the same.
           </p>
         </section>
       </div>
